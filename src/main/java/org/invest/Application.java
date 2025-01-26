f5da5a746bda7aa14ed6de274ae8101b142b29ae
Marsel1112 <maldarov243@gmail.com>
2025-01-26 23:21:53 +0300

    Добавил переменныые окружения

--- src/main/java/org/invest/Application.java
+++ src/main/java/org/invest/Application.java
@@ -1,5 +1,6 @@
 package org.invest;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients
 public class Application {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("POSTGRES_USERNAME", dotenv.get("POSTGRES_USERNAME"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+        System.setProperty("SECRET", dotenv.get("SECRET"));
+        System.setProperty("POLYGON_TOKEN", dotenv.get("POLYGON_TOKEN"));
+
         SpringApplication.run(Application.class, args);
     }
 }
