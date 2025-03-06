7a2a8e01d166feb1a996eae38d321e71c2e75df1
Marsel1112 <maldarov243@gmail.com>
2025-03-06 17:10:53 +0300

    Мелкие исправления Скрыл секретики

--- src/main/java/org/invest/Application.java
+++ src/main/java/org/invest/Application.java
@@ -1,23 +1,13 @@
 package org.invest;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableFeignClients
 public class Application {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("POSTGRES_USERNAME", dotenv.get("POSTGRES_USERNAME"));
-        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-        System.setProperty("SECRET", dotenv.get("SECRET"));
-        System.setProperty("POLYGON_TOKEN", dotenv.get("POLYGON_TOKEN"));
-
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/org/invest/controler/UserController.java
+++ src/main/java/org/invest/controler/UserController.java
@@ -1,4 +1,3 @@
-
 package org.invest.controler;
 
 import lombok.RequiredArgsConstructor;
